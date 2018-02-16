5f5414ecc170cb4bed3f335f679143449e57f618
Adam Johnson <me@adamj.eu>
2018-02-16 10:38:42 -0500

    Add python_requires to setup.py

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
     install_requires=[
         'flake8!=3.2.0',
     ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     license="ISCL",
     zip_safe=False,
     keywords='flake8_tidy_imports',
